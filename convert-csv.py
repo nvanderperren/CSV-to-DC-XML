9af819eb98895e76d757461bab057b0c4c70f63d
nvanderperren <ik.ben.nas@gmail.com>
2019-12-23 12:31:22 +0100

    add key check

--- convert-csv.py
+++ convert-csv.py
@@ -9,31 +9,31 @@ from argparse import ArgumentParser
 # functions
 def create_base_xml(fieldnames):
     # function variables
-    XMLNS = 'http://example.org/oudenburg-dc'
-    XSI = 'http://www.w3.org/2001/XMLSchema-instance'
-    DCTERMS = 'http://purl.org/dc/terms/'
-    SCHEMA = 'http://example.org/oudenburg-dc oudenburg-record.xsd http://purl.org/dc/terms/ http://dublincore.org/schemas/xmls/qdc/2008/02/11/dcterms.xsd'
-    MARCREL = 'http://www.loc.gov/loc.terms/relators/'
-    CLD = 'http://purl.org/cld/terms/'
-    DC = 'http://purl.org/dc/elements/1.1/'
+    xmlns = 'http://example.org/oudenburg-dc'
+    xsi = 'http://www.w3.org/2001/XMLschema-instance'
+    dcterms = 'http://purl.org/dc/terms/'
+    schema = 'http://example.org/oudenburg-dc oudenburg-record.xsd http://purl.org/dc/terms/ http://dublincore.org/schemas/xmls/qdc/2008/02/11/dcterms.xsd'
+    marcrel = 'http://www.loc.gov/loc.terms/relators/'
+    cld = 'http://purl.org/cld/terms/'
+    dc = 'http://purl.org/dc/elements/1.1/'
 
     root = Element('record')
-    root.set('xmlns', XMLNS)
-    root.set('xmlns:xsi', XSI)
+    root.set('xmlns', xmlns)
+    root.set('xmlns:xsi', xsi)
     
     if 'dc:creator' in fieldnames:
-        root.set('xmlns:dc', DC)
-        SCHEMA += ' http://purl.org/dc/elements/1.1/ http://dublincore.org/schemas/xmls/qdc/2008/02/11/dc.xsd'
+        root.set('xmlns:dc', dc)
+        schema += ' http://purl.org/dc/elements/1.1/ http://dublincore.org/schemas/xmls/qdc/2008/02/11/dc.xsd'
 
-    root.set('xmlns:dcterms', DCTERMS)
+    root.set('xmlns:dcterms', dcterms)
 
     if 'marcrel:own' in fieldnames:
-        root.set('xmlns:marcrel', MARCREL)
+        root.set('xmlns:marcrel', marcrel)
     
     if 'cld:itemformat' in fieldnames or 'cld:isLocatedAt' in fieldnames:
-        root.set('xmlns:cld', CLD)
+        root.set('xmlns:cld', cld)
 
-    root.set('xsi:schemaLocation', SCHEMA)
+    root.set('xsi:schemaLocation', schema)
     return root
 
 def create_dc_xml(file):
@@ -42,8 +42,10 @@ def create_dc_xml(file):
         xml = create_base_xml(csvreader.fieldnames)
         for row in csvreader:
             for key, value in row.items():
-                child = SubElement(xml, key)
-                child.text = value
+                if key.startswith('dc:') or key.startswith('dcterms:') or \
+                key.startswith('cld:') or key.startswith('marcrel:'):
+                    child = SubElement(xml, key)
+                    child.text = value
             mydata = prettify(xml)
             filename = f"{row['dcterms:identifier']}.xml"
             print_xml(filename, mydata)
